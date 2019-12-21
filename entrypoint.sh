682a46651e1f29e589d0c1bfd9ec6cfaf77440ac
JuniorJPDJ <git@juniorjpdj.pl>
2019-12-21 14:12:01 +0100

    Added autokill (use with compose to autorestart)

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,7 @@ rm -f /tmp/.X99-lock
 Xvfb -ac -screen scrn 1024x768x24 :99.0 &
 export DISPLAY=:99.0
 
+# autokill container after 24h (force restart, as it crashes after few days and doesnt kill itself)
+(sleep $((24*60*60)) ; echo 24h uptime - Killing ; kill $$) &
+
 exec "$@"
